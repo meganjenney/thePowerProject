67d44af4d41891fa87c4d24e9bbe490c2e63bb1c
Stephanie Bentley <sc.bentley@yahoo.com>
2022-04-04 17:44:15 -0400

    Add house module

--- house.erl
+++ house.erl
@@ -1,2 +1,22 @@
--module(house)
--export([])
+-module(house).
+-export([start/1]).
+
+-export([loop/1]).
+
+
+start(MaxPower) -> 
+    % TODO: Decide whether to spawn separate process for house
+    spawn(?MODULE, loop, [{MaxPower, 0}]).
+
+
+% Message receiving loop with debug code
+loop(CurrentState) -> 
+    erlang:display(CurrentState),
+    receive
+        {exit} -> 
+            erlang:display("Ending");
+        {Other} ->
+            io:format("Received: ~s~n", [Other]),
+            loop(CurrentState)
+    end.
+
