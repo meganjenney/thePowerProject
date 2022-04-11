7e43009f164e1738fd289091e1dedcc0f4fa22da
sbcohen2000 <sbcohen2000@gmail.com>
2022-04-11 19:17:27 -0400

    Add HTTP endpoint parsing and basic info endpoint

--- httpserver.erl
+++ httpserver.erl
@@ -15,23 +15,40 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -module(httpserver).
--export([start/1]).
+-export([start/2]).
 
 % based on https://stackoverflow.com/a/2219330/13492694
-start(Port) ->
+start(Port, HousePid) ->
     {ok, IndexBinary} = file:read_file("./index.html"),
+    State = {IndexBinary, HousePid},
     io:format("Starting on port ~p\n", [Port]),
-    spawn(fun () -> {ok, Sock} = gen_tcp:listen(Port, [{active, true}]),
-		    loop(Sock, IndexBinary) end).
+    spawn(fun () -> {ok, Sock} = gen_tcp:listen(Port, [{active, false}]),
+		    loop(Sock, State) end).
 
-loop(Sock, IndexBinary) ->
+loop(Sock, State) ->
     {ok, Conn} = gen_tcp:accept(Sock),
-    Handler = spawn(fun () -> handle(Conn, IndexBinary) end),
+    Handler = spawn(fun () -> handle(Conn, State) end),
     gen_tcp:controlling_process(Conn, Handler),
-    loop(Sock, IndexBinary).
+    loop(Sock, State).
 
-handle(Conn, IndexBinary) ->
-    gen_tcp:send(Conn, response(IndexBinary)),
+handle(Conn, State) ->
+    {IndexBinary, HousePid} = State,
+    {ok, Bin} = gen_tcp:recv(Conn, 0),
+    {Method, Endpoint} =
+	case re:run(Bin, "(?<METHOD>GET|POST) /(?<ENDPOINT>[[:alnum:]]*)", 
+		    [{capture,['METHOD','ENDPOINT'],list}]) of
+	    {match, [M, E]} -> {M, E};
+	    _ -> io:format("no match~n")
+	end,
+    case {Method, Endpoint} of
+	{"GET", []} ->
+	    gen_tcp:send(Conn, response(IndexBinary));
+	{"GET", "info"} ->
+	    Response = io_lib:format("~p", [house:get_info(HousePid)]),
+	    gen_tcp:send(Conn, Response);
+	_ ->
+	    gen_tcp:send(Conn, "HTTP/1.0 404 Not Found")
+    end,
     gen_tcp:close(Conn).
 
 response(B) ->
@@ -39,3 +56,4 @@ response(B) ->
       io_lib:fwrite(
 	"HTTP/1.0 200 Ok\nContent-Type: text/html\nContent-Length: ~p\n\n~s",
 	[size(B), B])).
+
